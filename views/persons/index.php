3faf239c48978b7dedfad5cf55d00cfd4476fb01
hackl <thomas.hackl@uni-passau.de>
2016-12-12 15:31:19 +0100

    bulk action in dialog

--- views/persons/index.php
+++ views/persons/index.php
@@ -1,6 +1,6 @@
 <?php if ($persons || $filter) : ?>
     <?= $this->render_partial('persons/_filter') ?>
 <?php endif ?>
-<form action="<?= $controller->url_for('persons/bulk') ?>" method="post">
+<form action="<?= $controller->url_for('persons/bulk') ?>" method="post" data-dialog>
     <div id="luna-data" data-type="persons" data-update-url="<?= $controller->url_for('persons/load_persons') ?>"></div>
 </form>

086cc7dbe5f09387a3957df3d42c597c94b182ed
Thomas Hackl <thomas.hackl@uni-passau.de>
2019-10-16 17:05:36 +0200

    set new IDs for existing entries.

--- migrations/19_last_contact_documents.php
+++ migrations/19_last_contact_documents.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once(__DIR__ . '/../models/LunaCompanyLastContact.php');
+
 /**
  * Adds a field and adjusts primary key for last contacts.
  */
@@ -7,6 +9,7 @@
 class LastContactDocuments extends Migration {
 
     public function up() {
+
         DBManager::get()->execute("ALTER TABLE `luna_company_last_contact` 
             DROP PRIMARY KEY");
 
@@ -16,7 +19,25 @@ class LastContactDocuments extends Migration {
         DBManager::get()->execute("ALTER TABLE `luna_company_last_contact`
             ADD PRIMARY KEY (`contact_id`)");
 
+        // Get all entries - they will need new IDs.
+        $entries = DBManager::get()->fetchAll("SELECT * FROM `luna_company_last_contact`");
+
         SimpleORMap::expireTableScheme();
+
+        DBManager::get()->execute("TRUNCATE TABLE `luna_company_last_contact`");
+
+        // Generate new IDs for each entry.
+        foreach ($entries as $entry) {
+            $one = new LunaCompanyLastContact();
+            $one->user_id = $entry['user_id'];
+            $one->company_id = $entry['company_id'];
+            $one->date = $entry['date'];
+            $one->contact = $entry['contact'];
+            $one->notes = $entry['notes'];
+            $one->mkdate = $entry['mkdate'];
+            $one->chdate = $entry['chdate'];
+            $one->store();
+        }
     }
 
     public function down() {
